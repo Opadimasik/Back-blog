741057b684ba4b4102988b7969023f86e11246b7
Дмитрий Фофанцев <kfk.551@yandex.ru>
2023-11-28 20:35:30 +0700

    Add all usable headers: 200,400,401,403,404,500

--- helpers/header.php
+++ helpers/header.php
@@ -8,6 +8,22 @@ function setHTTPStatus($status="200",$message=null)
         case"200":
             $status = "HTTP/1.0 200 OK";
             break;
+            case "400":
+                $status = "HTTP/1.0 400 Bad Request";
+                break;
+            case "401":
+                $status = "HTTP/1.0 401 Unauthorized";
+                break;
+            case "403":
+                $status = "HTTP/1.0 403 Forbidden";
+                break;
+            case "404":
+                $status = "HTTP/1.0 404 Not Found";
+                break;
+            
+            case "500":
+                $status = "HTTP/1.0 500 InternalServerError";
+                break;
     }
 
     header($status);
