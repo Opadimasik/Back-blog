33ecab3070b407492cd4894f2d05f785041a62a4
Дмитрий Фофанцев <kfk.551@yandex.ru>
2023-11-29 13:00:01 +0700

    Add 2 function: -for spoil token in db -for vaildate token

--- routers/accountRequest/logout.php
+++ routers/accountRequest/logout.php
@@ -6,11 +6,12 @@ function accountLogout()
     $token = getBearerToken();
     $tokenUntilTime = $Link->query("SELECT `validUntil` FROM `token` WHERE value='$token'");
 
-    if ($tokenUntilTime->num_rows > 0) {
-        $row = $tokenUntilTime->fetch_assoc();
-        $randomValue = random_int(0, 1000000);
-        $Link->query("UPDATE `token` SET `value`=$randomValue WHERE value='$token'");
-    } else {
+    if ($tokenUntilTime->num_rows > 0) 
+    {
+        spoilToken($token);
+    } 
+    else 
+    {
         setHTTPStatus("401");
     }
 }
