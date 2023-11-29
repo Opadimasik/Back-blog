6a2baad44e91d74371c4d93217dbee2fd3617602
Дмитрий Фофанцев <kfk.551@yandex.ru>
2023-11-29 13:08:20 +0700

    Modified route function: -check vailid token -else 401

--- routers/accountRequest/putProfile.php
+++ routers/accountRequest/putProfile.php
@@ -1,29 +1,33 @@
 <?php
 function accountPutProfile($formData)
 {
-    // необходимо добавить проверку на дату токена
+  
     global $Link;
     $token = getBearerToken();
-    $currentTime = date("Y-m-d H:i:s");
-    
-    $result = $Link->query("SELECT `accountID` FROM `token` WHERE value='$token'");
-
-    if ($result->num_rows > 0) 
+    if (isTokenValid($token)) 
     {
-        $row = $result->fetch_assoc();
-        $accountID = $row['accountID'];
-
-        foreach ($formData as $key => $rowValue)
+        $result = $Link->query("SELECT `accountID` FROM `token` WHERE value='$token'");
+        if ($result->num_rows > 0) 
         {
-            $Link->query("UPDATE `account` SET `$key`='$rowValue' WHERE id='$accountID'");
-            if ($Link->error != "")
+            $row = $result->fetch_assoc();
+            $accountID = $row['accountID'];
+
+            foreach ($formData as $key => $rowValue)
             {
-                setHTTPStatus("400", $Link->error);
+                $Link->query("UPDATE `account` SET `$key`='$rowValue' WHERE id='$accountID'");
+                if ($Link->error != "")
+                {
+                    setHTTPStatus("400", $Link->error);
+                }
             }
+        } else 
+        {
+            setHTTPStatus("401");
         }
-    } else 
+    }
+    else
     {
-        setHTTPStatus("401");
+        setHTTPStatus("401", "The token has expired.");
     }
 }
 
