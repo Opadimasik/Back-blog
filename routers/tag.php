3bb51d0144572ed89cc12af885177f55217ee07b
Дмитрий Фофанцев <kfk.551@yandex.ru>
2023-12-01 02:23:56 +0700

    Add function for get tags
    -request to db
    -echo result

--- routers/tag.php
+++ routers/tag.php
@@ -3,6 +3,13 @@ function route($method, $urlData, $formData)
 { 
     global $Link;
     $result = $Link->query("SELECT * FROM tag")->fetch_all(MYSQLI_ASSOC);
-    echo json_encode($result);
+    if(!is_null($result))
+    {
+        echo json_encode($result);
+    }
+    else
+    {
+        setHTTPStatus("500", "Server can not get list of tag");
+    }
 }
 ?>
