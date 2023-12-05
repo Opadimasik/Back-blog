0598ad46c004bc1f9c7234cc44cee1b7a26e8b81
Дмитрий Фофанцев <kfk.551@yandex.ru>
2023-12-06 01:03:31 +0700

    Add getCommentTree function
    -this commit adds the 'getCommentTree' function, which retrieves a tree of comments based on the provided commentId. The function uses a recursive query to fetch comments and their sub-comments from the 'comment' table, considering only active comments (where isDelete is false). The result is encoded in JSON format and returned.
    -added 'checkExistComment' function now checks for deleted comments

--- helpers/commentHelper.php
+++ helpers/commentHelper.php
@@ -2,7 +2,7 @@
 function checkExistComment($commentId)
 {
     global $Link;
-    $result = $Link->query("SELECT `content` FROM `comment` where `id`='$commentId'");
+    $result = $Link->query("SELECT `content` FROM `comment` where `id`='$commentId' and `isDelete`=0");
     if ($result->num_rows > 0) 
     {
         return true;
