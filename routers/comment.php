54ab5535e4ae6f5f4b86e6f2787c1fd5b5fbcc9e
Дмитрий Фофанцев <kfk.551@yandex.ru>
2023-12-05 23:56:58 +0700

    Add functions createComment and validateComment
    -this commit adds the 'createComment' function, which creates a new comment based on the provided form data. It checks the validity of the token, validates the input parameters, and inserts a new comment into the 'comment' table.
    -additionally, the 'validateComment' helper function is introduced to validate the comment parameters.

---
+++ routers/comment.php
@@ -0,0 +1,22 @@
+<?php
+include_once("helpers/postHelper.php");
+function route($method, $urlData, $formData) 
+{ //Обрабатываем запрос.
+    global $Link;
+    include_once("helpers/postHelper.php");
+    switch ($method) 
+    {
+        case "GET":
+            // include_once("commentRequest/");
+
+            // return;
+        case "DELETE":
+            // include_once("commentRequest/");
+
+            // return;
+        case "PUT":
+            // include_once("commentRequest/");
+
+            // return;
+    }
+}
---
+++ routers/commentRequest/commentPost.php
@@ -0,0 +1,109 @@
+<?php
+include_once("helpers/commentHelper.php");
+function createComment($formData)
+{
+    global $Link;
+    $token = getBearerToken();
+    if (isTokenValid($token)) 
+    {
+        $authorIdResult = $Link->query("SELECT `accountID` FROM `token` WHERE value='$token'");
+        $authorIdData = $authorIdResult->fetch_assoc();
+        if(!is_null($authorIdData))
+        {
+            $postId = getParams("id");
+            if(validateComment($formData,$postId))
+            {
+                $authorId = $authorIdData['accountID'];
+                $authorResult = $Link->query("SELECT `fullName` FROM `author` where accountId='$authorId'");
+                $authorData = $authorResult->fetch_assoc();
+                $author = $authorData['fullName'];
+                $content=$formData->content;
+                $parentId = $formData->parentId;
+                $commentId = bin2hex(random_bytes(16));
+                $commentQuery = $Link->query("INSERT INTO comment (
+                    `id`,
+                    `authorId`,
+                    `author`,
+                    `content`,
+                    `parentId`,
+                    `postId`
+                ) VALUES (
+                    '$commentId',
+                    '$authorId',
+                    '$author',
+                    '$content',
+                    '$parentId',
+                    '$postId'
+                )");
+                if(!$commentQuery)
+                {
+                    setHTTPStatus("400","Problem with creating comment:"."$Link->error");
+                    return;
+                }
+                if (!is_null($parentId))
+                {
+                    $subCommentQuery = $Link->query("UPDATE comment SET subComments = subComments + 1 WHERE id = '$parentId'");
+                    if(!$subCommentQuery)
+                    {
+                        setHTTPStatus("400","$Link->error");
+                        return;
+                    }
+                }
+                
+            //     $test = $Link->query("WITH RECURSIVE CommentTree AS (
+            //         SELECT id, parentId, content
+            //         FROM comment
+            //         WHERE id = '595f1c432cd575ae673e58b1992d27fb'
+            //         UNION
+            //         SELECT c.id, c.parentId, c.content
+            //         FROM comment c
+            //         JOIN CommentTree ct ON c.parentId = ct.id
+            //     )
+            //     SELECT * FROM CommentTree
+            //     WHERE id <> '595f1c432cd575ae673e58b1992d27fb';");
+            //     $testRes = $test->fetch_all(MYSQLI_ASSOC);
+            //     echo json_encode($testRes);
+            }
+            else return;
+        }
+        else
+        {
+            setHTTPStatus("400","Account id can not be find");
+            return;
+        }
+    }
+    else
+    {
+        setHTTPStatus("401", "The token has expired.");
+    }
+}
+
+function validateComment($formData,$postId)
+{
+    
+    if(is_null($postId))
+    {
+        setHTTPStatus('400','The '.'postId'.' parameter was passed incorrectly');
+        return false;
+    }
+    if(!checkExistPost($postId))
+    {
+        setHTTPStatus('404', "Post not find. Check your postId");
+        return false;
+    }
+    if(!validateStringNoteLess(strlen($formData->content),1))
+    {
+        setHTTPStatus("400","Content very short, minimum leght 1 or it's not there.");
+        return false;
+    }
+    if(!is_null($formData->parentId))
+    {
+        if(!checkExistComment($formData->parentId))
+        {
+            setHTTPStatus("404","There is no such post that was passed to parentId. Try checking the data.");
+            return false;
+        }
+        return true;
+    }
+    return true;
+}
