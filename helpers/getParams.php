40336f9bd7e44931a9494f49661b852e8b9d9b96
Дмитрий Фофанцев <kfk.551@yandex.ru>
2023-12-07 03:46:45 +0700

    Add getParamsForRepetition function
    - Introduced getParamsForRepetition function for handling repeating query parameters
    - Implemented parsing of the request URI and extraction of the specified parameter
    - Ensured the function returns an array of sanitized and trimmed values
    - Improved code readability with comments for better understanding
    - Included error handling to return an empty array if the parameter is not present

--- helpers/getParams.php
+++ helpers/getParams.php
@@ -6,3 +6,19 @@ function getParams($param)
     parse_str( $parts['query'] , $query );
     return $query["$param"];
 }
+function getParamsForRepetition($param)
+{
+    $urlAll = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
+    $parts = parse_url($urlAll);
+    parse_str($parts['query'], $query);
+
+    if (isset($query[$param])) {
+        $paramValue = is_array($query[$param]) ? $query[$param] : [$query[$param]];
+
+        // обрезаю и нормализую
+        $paramValues = array_map('htmlspecialchars', array_map('trim', $paramValue));
+        return $paramValues;
+    } else {
+        return [];
+    }
+}
--- helpers/postHelper.php
+++ helpers/postHelper.php
@@ -9,3 +9,27 @@ function checkExistPost($postId)
     }
     else return false;
 }
+function checkExistTags($tags)
+{
+    global $Link;
+    $tagsString = implode("', '", $tags);
+    $result = $Link->query("SELECT id FROM tag WHERE id IN ('$tagsString')");
+    if (!$result) 
+    {
+        setHTTPStatus('500', $Link->error);
+        return false;
+    }
+    $existingTags = [];
+    while ($row = $result->fetch_assoc()) 
+    {
+        $existingTags[] = $row['id'];
+    }
+    $missingTags = array_diff($tags, $existingTags);
+    if (!empty($missingTags)) 
+    {
+        $missingTagsString = implode(', ', $missingTags);
+        setHTTPStatus('404', "Tag(s) not exist: $missingTagsString");
+        return false;
+    }
+    return true;
+}
