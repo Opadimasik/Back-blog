ac174716af8cdfda79fc6f3f31188808716d0087
Дмитрий Фофанцев <kfk.551@yandex.ru>
2023-12-07 21:29:19 +0700

    Updated profile output
    - now the profile is displayed without a password

--- routers/accountRequest/getProfile.php
+++ routers/accountRequest/getProfile.php
@@ -1,18 +1,15 @@
 <?php
 function accountGetProfile()
 {
-    //нужно добавить проверку токена на соответсвие времени
     global $Link;
     $token = getBearerToken();
-    $currentTime = date("Y-m-d H:i:s");
     $result = $Link->query("SELECT `accountID` FROM `token` WHERE value='$token'");
 
     if ($result->num_rows > 0) 
     {
         $row = $result->fetch_assoc();
         $accountID = $row['accountID'];
-
-        $accountData = $Link->query("SELECT * FROM `account` WHERE id='$accountID'");
+        $accountData = $Link->query("SELECT fullName,email,birthDate,gender,phoneNumber,id,created FROM `account` WHERE id='$accountID'");
         echo json_encode($accountData->fetch_assoc());
     } else 
     {
