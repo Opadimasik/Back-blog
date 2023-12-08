9ebff6fce6ee02716101925db66a39e2a67c6010
Дмитрий Фофанцев <kfk.551@yandex.ru>
2023-12-08 14:13:16 +0700

    Add function getUserRole
    -This commit introduces a function getUserRole($communityId) that retrieves the user role for a given community ID.
    -The function checks the validity of the bearer token, fetches the account ID associated with the token from the 'token' table, and then queries the 'community_role' table to determine the user's role in the specified community.

--- routers/communityRequest/getRole.php
+++ routers/communityRequest/getRole.php
@@ -1,5 +1,5 @@
 <?php
-function getUserCommuninty()
+function getUserRole($communityId)
 {
     global $Link;
     $token = getBearerToken();
@@ -10,11 +10,11 @@ function getUserCommuninty()
         {
             $row = $result->fetch_assoc();
             $accountID = $row['accountID'];
+            $id = "$communityId"."\r\n";
             $commutiyRoleUserQuery = $Link->query("
-                SELECT MIN(role) AS role, userId, communityId
-                FROM community_role
-                WHERE userId='$accountID'
-                GROUP BY communityId
+            SELECT MIN(`role`) as role
+            FROM community_role
+            WHERE `userId`='$accountID' AND `communityId`='$id'
             ")->fetch_all(MYSQLI_ASSOC);
             if(!is_null($commutiyRoleUserQuery))
             {
@@ -22,7 +22,7 @@ function getUserCommuninty()
             }
             else
             {
-                setHTTPStatus("404","User roles were not found, the user is probably not a subscriber or administrator in any of the communities");
+                setHTTPStatus("404","User roles were not found, the user is probably not a subscriber or administrator of this community");
             }
         } 
         else 
